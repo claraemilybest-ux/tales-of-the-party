ce5ee64879030983a84aa636e1715f5b341b7b91
Clara Best <clara@Mac.lan>
2026-07-11 20:00:23 -0600

    cors has been added

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import "dotenv/config"
+import { clerkMiddleware } from '@clerk/express'
+import cors from "cors";
 
 import User from "./models/User.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
